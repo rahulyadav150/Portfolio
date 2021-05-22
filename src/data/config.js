f7e9090b5d8272143951aa119b07354f15f759bc
rahul <ry887176@gmail.com>
2021-05-23 04:41:48 +0530

    final check

--- src/data/config.js
+++ src/data/config.js
@@ -3,7 +3,7 @@ module.exports = {
   // TODO: change your profile information here
   name: "Rahul Yadav",
   greeting: "Hey 👋",
-  greetingDescription: "I'm Rahul Yadav , a software  developer from India ",
+  greetingDescription: "I'm Rahul Yadav , a passionate Software Developer from India!",
   githubUrl: "https://github.com/rahulyadav150",
   linkedinUrl: "https://www.linkedin.com/in/rahul-yadav-20b8611a9/",
   cvLink: "https://drive.google.com/file/d/1_vsbO-9XwMcONyo5f86-e72H6Y1Z0bYM/view",
